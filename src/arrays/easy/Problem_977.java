4b34aae5aff157402dd643cfc34d597170cbb9e4
chandankj399 <chandankj399@gmail.com>
2020-05-10 11:29:49 -0700

    Sorted Squares

---
+++ src/arrays/easy/Problem_977.java
@@ -0,0 +1,13 @@
+package arrays.easy;
+
+import java.util.Arrays;
+
+public class Problem_977 {
+	public int[] sortedSquares(int[] A) {
+        for (int i=0; i<A.length; i++) {
+            A[i] = A[i]*A[i];
+        }
+        Arrays.sort(A);
+        return A;
+    }
+}
