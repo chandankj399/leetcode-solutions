d2a75c45dc4c60b1bb3de1356bafcc572927967e
chandankj399 <chandankj399@gmail.com>
2020-05-11 13:19:15 -0700

    Transpose a matrix

---
+++ src/arrays/easy/Problem_867.java
@@ -0,0 +1,17 @@
+package arrays.easy;
+
+public class Problem_867 {
+	class Solution {
+	    public int[][] transpose(int[][] A) {
+	        int[][] newArr = new int[A[0].length][A.length];
+	        for(int i = 0; i < A.length; i++){
+	            for (int j = 0; j < A[0].length; j++){
+	                newArr[j][i] = A[i][j];
+	            }
+	        }
+	        
+	        return newArr;
+	    }
+	}
+
+}
