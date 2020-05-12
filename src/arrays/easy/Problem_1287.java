699c374af2586ee31480d0ac032279a8b7075061
chandankj399 <chandankj399@gmail.com>
2020-05-12 12:18:33 -0700

    Find special integer

---
+++ src/arrays/easy/Problem_1287.java
@@ -0,0 +1,21 @@
+package arrays.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Problem_1287 {
+	class Solution {
+	    public int findSpecialInteger(int[] arr) {
+	        Map<Integer,Integer> map = new HashMap<>();
+	        for(int i : arr){
+	            map.put(i, map.getOrDefault(i,0) +1);
+	            if (map.get(i) > arr.length/4){
+	                return i;
+	            }
+	        }
+	        
+	       
+	        return -1;
+	    }
+	}
+}
