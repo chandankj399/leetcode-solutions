4a5aad6e674a019d4bd720ad312123dac6b05eaf
chandankj399 <chandankj399@gmail.com>
2020-05-13 11:25:37 -0700

    Merge sorted arrays

---
+++ src/arrays/easy/Problem_88.java
@@ -0,0 +1,26 @@
+package arrays.easy;
+
+public class Problem_88 {
+
+public void merge(int[] nums1, int m, int[] nums2, int n) {
+		int[] res = nums1.clone();
+		int i = 0, j = 0, count = 0;
+		while (i < m && j < n) {
+
+			if (res[i] <= nums2[j]) {
+				nums1[count++] = res[i++];
+			} else {
+				nums1[count++] = nums2[j++];
+			}
+		}
+		while (i < m) {
+			nums1[count++] = res[i++];
+		}
+		while (j < n) {
+			nums1[count++] = nums2[j++];
+		}
+	}
+
+
+
+}
